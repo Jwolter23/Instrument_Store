3bc8847e9b88e6138bfd731c820855f7a1b323c7
Ryan Saucier <ryanyoubum@gmail.com>
2022-12-05 17:15:11 -0500

    posting

--- client/src/components/Electric.jsx
+++ client/src/components/Electric.jsx
@@ -1,10 +1,20 @@
+import { useState,useEffect } from "react"
+import axios from "axios"
+
 export default function Electric (props) {
-    console.log(props.instruments[1])
+console.log(props)
+    if (!props.instruments) {
+        return <h2>Loading please wait.</h2>
+    } else {         
     return(
-        <div>
-            <h1>
-                
-            </h1>
+        <div className="grid">
+        <div className="instrument">
+            <h3>{props.instruments[0].brand}</h3>
+            <h3>{props.instruments[0].model}</h3>
+            <img className="instrument-image" src={props.instruments[0].image}></img>
+            </div>
         </div>
     )
+}
+
 }
