ba13015867a9618c1e9732c54845bd1e8ce5bc41
Ryan Saucier <ryanyoubum@gmail.com>
2022-12-05 17:20:54 -0500

    posting

--- client/src/components/Electric.jsx
+++ client/src/components/Electric.jsx
@@ -8,10 +8,31 @@ console.log(props)
     } else {         
     return(
         <div className="grid">
-        <div className="instrument">
+            <div className="instrument">
             <h3>{props.instruments[0].brand}</h3>
             <h3>{props.instruments[0].model}</h3>
             <img className="instrument-image" src={props.instruments[0].image}></img>
+            <h3>{props.instruments[0].color}</h3>
+            <h3>{props.instruments[0].type}</h3>
+            <h3>${props.instruments[0].price}</h3>
+            </div>
+
+            <div className="instrument">
+            <h3>{props.instruments[1].brand}</h3>
+            <h3>{props.instruments[1].model}</h3>
+            <img className="instrument-image" src={props.instruments[1].image}></img>
+            <h3>{props.instruments[1].color}</h3>
+            <h3>{props.instruments[1].type}</h3>
+            <h3>${props.instruments[1].price}</h3>
+            </div>
+            
+            <div className="instrument">
+            <h3>{props.instruments[2].brand}</h3>
+            <h3>{props.instruments[2].model}</h3>
+            <img className="instrument-image" src={props.instruments[2].image}></img>
+            <h3>{props.instruments[2].color}</h3>
+            <h3>{props.instruments[2].type}</h3>
+            <h3>${props.instruments[2].price}</h3>
             </div>
         </div>
     )
