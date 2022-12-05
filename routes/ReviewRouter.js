606b0108e459dc9ec3c98ca66e3364cded6f6902
Greg Swan <gmswan99@gmail.com>
2022-12-05 13:52:14 -0500

    uncomment getAllReviews
    '

--- routes/ReviewRouter.js
+++ routes/ReviewRouter.js
@@ -2,7 +2,7 @@ const Router = require('express').Router()
 const controller = require('../controllers/ReviewController')
 
 
-// Router.get('/view', controller.getAllReviews)
+Router.get('/view', controller.getAllReviews)
 // Router.get('/view/:review_id', controller.GetReviewDetails)
 // Router.post('/:user_id', controller.AddReview)
 // Router.put('/:review_id', controller.UpdateReview)
