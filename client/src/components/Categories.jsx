190bacbf96f3f6bb9e1bdf195c2c41644df841e0
Ryan Saucier <ryanyoubum@gmail.com>
2022-12-05 13:46:21 -0500

    styling

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -1,42 +1,45 @@
+import { useNavigate } from "react-router"
+
 export default function Categories () {
+    let navigate = useNavigate();
     return (
         <div className="grid">
-            <div className="category c1">Electric Guitars
+            <div className="category c1" onClick={(e)=>navigate("/electric")}>Electric Guitars
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             </div>
-            <div className="category c2">Acoustic Guitars
+            <div className="category c2" onClick={(e)=>navigate("/acoustic")}>Acoustic Guitars
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             </div>
-            <div className="category c3">Bass Guitars
+            <div className="category c3" onClick={(e)=>navigate("/bass")}>Bass Guitars
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             </div>
-            <div className="category c4">Amplifyers
+            <div className="category c4" onClick={(e)=>navigate("/amps")} >Amplifyers
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             </div>
-            <div className="category c5">Pedals/Effects
+            <div className="category c5" onClick={(e)=>navigate("/pedalsandeffects")} >Pedals/Effects
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             </div>
-            <div className="category c6">All
+            <div className="category c6" onClick={(e)=>navigate("/all")} >All
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
