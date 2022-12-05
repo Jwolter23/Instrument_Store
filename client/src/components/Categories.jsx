da155e246cb7457cb20f38e3252394652c69c4e1
Ryan Saucier <ryanyoubum@gmail.com>
2022-12-05 11:24:51 -0500

    styling grid

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -1,62 +1,49 @@
 export default function Categories () {
     return (
         <div className="grid">
-            <div className="category c1">Category 1
+            <div className="category c1">Electric Guitars
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             </div>
-            <div className="category c2">Category 1
+            <div className="category c2">Acoustic Guitars
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             </div>
-            <div className="category c3">Category 1
+            <div className="category c3">Bass Guitars
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             </div>
-            <div className="category c4">Category 1
+            <div className="category c4">Amplifyers
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             </div>
-            <div className="category c5">Category 1
+            <div className="category c5">Pedals/Effects
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             </div>
-            <div className="category c6">Category 1
+            <div className="category c6">All
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             <h3>DDDDDDDDDDD</h3>
             </div>
-            <div className="category c7">Category 1
-            <h3>DDDDDDDDDDD</h3>
-            <h3>DDDDDDDDDDD</h3>
-            <h3>DDDDDDDDDDD</h3>
-            <h3>DDDDDDDDDDD</h3>
-            <h3>DDDDDDDDDDD</h3>
-            </div>
-            <div className="category c8">Category 1
-            <h3>DDDDDDDDDDD</h3>
-            <h3>DDDDDDDDDDD</h3>
-            <h3>DDDDDDDDDDD</h3>
-            <h3>DDDDDDDDDDD</h3>
-            <h3>DDDDDDDDDDD</h3>
-            </div>
+
         </div>
         
         
