35c627d1b03ef9f37c168a3cbaa983b815256c09
Greg Swan <gmswan99@gmail.com>
2022-12-06 14:13:16 -0500

    Add about page and favicon

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -2,7 +2,28 @@ export default function About () {
     return(
         <div className="about-container">
             <div className="about-info">
-                <h1>INFO</h1>
+                <h1>Welcome to the Instrument Store!</h1>
+                <p>
+                The Instrument Store is an ecommerce store that focuses on selling the most popular musical instruments and equipment available today.</p>
+                
+                <p>Some of the top guitars that are currently available are electric guitars like the Gibson Les Paul Custom and ES-335, the Fender Stratocaster SRV and the Ibanez Steve Vai Signature. Our leading acoustic guitar line includes the Gibson Hummingbird and the Martin D-18.</p>
+
+                <p><img src="guitar2.jpg"></img>We also have a nice selection of bass guitars including the Fender Jazz Bass, Mustang and Precision bass. And for those Beatle fans, we have an affordable Epiphone Viola (Hofner) Bass.</p>
+
+                <p>In addition to guitars, we carry a full line of guitar amplifiers and guitar pedals from such classic brands as Marshall, Fender, Mesa Boogie, Vox, Boss, MXR, and much more.</p>
+
+                <h2>Website Overview</h2>
+
+                <p>We took great care and thought to create a website for our ecommerce store that was easy to navigate and produced an enjoyable user experience.</p>
+
+                <p>Users will be greeted by a login page with the option to log in and access their cart, or browse the website as a guest.</p>
+
+                <p>The Instruments tab will take users to a page focused on various instrument categories. Clicking on an instrument will give the user the ability to read more about it, see or add comments, and add the instrument to their cart.</p>
+
+                <p>The Comment section will show users a current list of comments.</p>
+
+                <p>The Cart section will show users which instruments they added to their cart, along with the sub total and total.</p>
+
             </div>
         </div>
     )
