4322db212a0508e779a7d42237c548913ef2f1ab
Jordan Wolter <jwolter456@gmail.com>
2022-12-06 10:08:26 -0500

    fixed add on reviews CRUD

---
+++ controllers/CommentController.js
@@ -0,0 +1,73 @@
+const {Comment} = require('../models')
+
+
+// C ......
+// R ......
+// U ......
+// D ......
+// Completed 
+
+
+
+
+
+const getAllComments = async (req, res) => {
+  try{
+    const comment = await Comment.findAll()
+    res.send(review)
+  } catch (error) {
+    throw error
+  }
+}
+
+const GetCommentDetails = async (req, res) => {
+  try {
+    const comment = await Review.findByPk(req.params.comment_id)
+    res.send(comment)
+  } catch (error) {
+    throw error
+  }
+}
+
+const AddComment = async (req, res) => {
+  try {
+    let userId = parseInt(req.params.user_id)
+    let commentId = parseInt(req.params.comment_id)
+    let commentBody = {userId, commentId, ...req.body}
+    let comment = await Comment.create(commentBody)
+    res.send(comment)
+  } catch (error) {
+    throw error
+  }
+}
+
+const UpdateComment = async (req, res) => {
+  try {
+    let commentId = parseInt(req.params.comment_id)
+    let updatedComment = await Comment.update(req.body, {
+      where: {id: commentId},
+      returning: true 
+    })
+    res.send(updatedComment)
+  } catch (error) {
+    throw error
+  }
+}
+
+const DeleteComment = async (req, res) => {
+  try {
+    let commentId = parseInt(req.params.comment_id)
+    await Comment.destroy({where: {id: commentId} })
+    res.send({message: `Deleted comment with an id of ${commentId}`})
+  } catch (error) {
+    throw error
+  }
+}
+
+module.exports = {
+  // GetCommentDetails,
+  // getAllComments,
+  AddComment,
+// UpdateComment,
+//   DeleteComment
+}
---
+++ routes/CommentRouter.js
@@ -0,0 +1,12 @@
+const Router = require("express").Router();
+const controller = require("../controllers/CommentController");
+
+//Router.get("/view", controller.getAllComments);
+// Router.get('/view/:review_id', controller.GetCommentDetails)
+
+//Router.post('/:user_id', controller.AddComment)
+
+// Router.put('/:review_id', controller.UpdateComment)
+// Router.delete('/:review_id', controller.DeleteComment)
+
+module.exports = Router;
