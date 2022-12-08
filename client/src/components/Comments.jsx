cd431525b980ac25223aea4f7f0654570b47fa2f
Ryan Saucier <ryanyoubum@gmail.com>
2022-12-08 15:09:52 -0500

    fixed edit

--- client/src/components/Comments.jsx
+++ client/src/components/Comments.jsx
@@ -8,13 +8,14 @@ import SignIn from "./SignIn"
 
 
 export default function Comments (props) {
+
     console.log(props)
     let navigate = useNavigate()
 
 
 
       function navComment () {
-        navigate(`/comments/${props.user.id}`)
+        navigate(`/comments/${props.selectedComment.id}`)
     }
 
     
@@ -32,7 +33,7 @@ console.log(props.user)
         setComments(response.data)
         
 
-       
+
         }
         getComments()
     }, [])
@@ -51,7 +52,7 @@ console.log(props.user)
                 <div className="grid-comments">
                    {
                     comments.map((comment, i) => (
-                        <div className="indiv-comments"key={comment.name}>
+                        <div className="indiv-comments"key={comment.name} onMouseOver={((e)=>props.setSelectedComment(comment))}>
                             <h2>{comment.username}</h2>
                             
                             <h3>{comment.content}</h3>
@@ -61,7 +62,7 @@ console.log(props.user)
                             {
                                 (comment.username === props.user.username) 
                                 ?                             
-                                <button className="edit-comment-button" onClick={navComment}>Edit</button>
+                                <button className="edit-comment-button"    onClick={navComment}>Edit</button>
                                 :
                                  null
                             }
@@ -69,7 +70,7 @@ console.log(props.user)
                                 (comment.username === props.user.username) 
                                 ?
                                 // <button className="delete-comment-button" onClick={handleSubmit(i)}>Delete</button>
-                                <DeleteComment comment={comment} />
+                                <DeleteComment selectedComment={props.selectedComment}comment={comment} />
                                 :
                                 null
                             }
