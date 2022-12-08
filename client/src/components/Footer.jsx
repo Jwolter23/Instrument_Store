e3de8daab4545484f7abcf48d8462dab10652549
Ryan Saucier <ryanyoubum@gmail.com>
2022-12-08 10:47:47 -0500

    some styling

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,28 @@
+import Github from "./githublogo.png"
 export default function Footer (){
     return(
         <div className="footer-container">
             <div className="footer">
-                <a href="" target="_blank" className="footer-item">Link</a>
-                <a href="" target="_blank" className="footer-item">Link</a>
+                <a href="https://github.com/Jwolter23/Instrument_Store" target="_blank" className="footer-item githublogo">
+                    <p className="color1">Repo</p> 
+                <img className="githublogo" src={Github}></img>
+                </a>
+                <a href="https://github.com/Gohmul" target="_blank" className="footer-item githublogo">
+                    <p className="color2">Ryan</p>
+                <img className="githublogo" src={Github}></img>
+                </a>
+                <a href="https://github.com/Jwolter23" target="_blank" className="footer-item githublogo">
+                    <p className="color3">Jordan</p>
+                <img className="githublogo" src={Github}></img>
+                </a>
+                <a href="https://github.com/swanmac" target="_blank" className="footer-item githublogo">
+                    <p className="color4">Greg</p> 
+                <img className="githublogo" src={Github}></img>
+                </a>
+                <a href="https://github.com/imdadhossain1" target="_blank" className="footer-item githublogo">
+                    <p className="color5">Imdad</p> 
+                <img className="githublogo" src={Github}></img>
+                </a>
             </div>
         </div>
     )
--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -1,17 +1,18 @@
-const Router = require('express').Router()
+const Router = require("express").Router();
 
-const ReviewRouter = require('./ReviewRouter')
-const InstrumentRouter = require('./InstrumentRouter')
-const CommentRouter = require('./CommentRouter')
-const CartRouter = require('./CartRouter')
+const ReviewRouter = require("./ReviewRouter");
+const InstrumentRouter = require("./InstrumentRouter");
+const CommentRouter = require("./CommentRouter");
+const CartRouter = require("./CartRouter");
+const AuthRouter = require("./AuthRouter");
+const PostRouter = require("./PostRouter");
 
+Router.use("/reviews", ReviewRouter);
+Router.use("/instruments", InstrumentRouter);
+Router.use("/comments", CommentRouter);
+Router.use("/carts", CartRouter);
 
-Router.use('/reviews', ReviewRouter)
-Router.use('/instruments', InstrumentRouter)
-Router.use('/comments', CommentRouter)
-Router.use('/carts', CartRouter)
+Router.use("/auth", AuthRouter);
+Router.use("/posts", PostRouter);
 
-
-
-
-module.exports = Router
+module.exports = Router;
