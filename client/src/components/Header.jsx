7ca0970dbaa9906e25dfa877f87d6d5e80935481
Ryan Saucier <ryanyoubum@gmail.com>
2022-12-08 11:21:04 -0500

    more styling and header onclick added

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,17 @@
 import Nav from "./Nav"
 import Welcome from "./Welcome"
+import { useNavigate } from "react-router"
 
 export default function Header (props) {
+    let navigate = useNavigate()
+
+    function Home () {
+        navigate('/')
+    }
     return(
         <div className="header">
         <Welcome authenticated={props.authenticated} user={props.user} handleLogOut={props.handleLogOut}/>
-        <h1 className="header-text">Instrument Store</h1>
+        <h1 className="header-text"onClick={(e)=> Home()}>Instrument Store</h1>
         <div className="header-nav">
             <Nav/>
         </div>
